e9d8f101d3553fb61a1cffd98894f0c9a92df598
Julián Rojas <juliandra140201@gmail.com>
2023-06-15 17:43:03 -0500

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+import { Navbar } from 'reactstrap';
+export default Navbar = () => {
+    return(
+        <Navbar></Navbar>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//Seleccionamos el DOM y lo guardamos en una constante
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Crear componentes a partir de funciones
+function Greeting () {
+    return <div>
+        <h1>Esto es un componente</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+    </div>
+}
+root.render(<div>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
+</div>)
